37b02c8fd63fe41eda58c752923f2c7fbe01c370
Joowon Yang <jywleah1101@yonsei.ac.kr>
2024-01-03 18:37:49 +0900

    updates
    이미지_깨짐현상_success+배경_전환_success

--- Assets/Scripts/GroundCtrl.cs
+++ Assets/Scripts/GroundCtrl.cs
@@ -4,52 +4,78 @@ using UnityEngine;
 
 public class GroundCtrl : MonoBehaviour
 {
-    public Material[] backgrounds;  // ��� Material �迭
-    public float changeInterval = 10.0f;  // ��� ���� �ֱ� (��)
+    //updown speed
+    public float scrollSpeedY;
 
+    // Saving several background materials
+    public Material[] backgrounds;  
+    
+    // time interval of changing the background
+    public float changeInterval = 10.0f;  
+
+    // refering the Renderer component attached on the game object
     private Renderer backgroundRenderer;
-    private int currentBackgroundIndex = 0;
-    private int nextBackgroundIndex = 1; // add next background index 
 
-    private float transitionTimer = 0.0f;
+    // saving the index of current background
+    private int currentBackgroundIndex = 0;
+
+    // add next background index 
+    //private int nextBackgroundIndex = 1; 
 
     void Start()
     {
         backgroundRenderer = GetComponent<Renderer>();
 
+        // initialize the current background to the first material
         backgroundRenderer.material = backgrounds[currentBackgroundIndex];
 
-//        StartCoroutine(ChangeBackground());
+        StartCoroutine(ChangeBackground());
     }
 
-    /* IEnumerator ChangeBackground()
+      IEnumerator ChangeBackground()
     {
         while (true)
         {
-            yield return new WaitForSeconds(changeInterval);
+            //yield return new WaitForSeconds(changeInterval);
 
-            // ���� ������� ����
-            currentBackgroundIndex = (currentBackgroundIndex + 1) % backgrounds.Length;
-            backgroundRenderer.material = backgrounds[currentBackgroundIndex];
+            //backgroundRenderer.material = backgrounds[currentBackgroundIndex];
+            backgroundRenderer.material.Lerp(backgrounds[currentBackgroundIndex], backgrounds[currentBackgroundIndex+1], changeInterval);
+            currentBackgroundIndex = currentBackgroundIndex + 1;
+            yield return null;
+
+            if (currentBackgroundIndex == backgrounds.Length - 1 )
+            {
+                currentBackgroundIndex = -1;
+            }
         
         }
-    }*/
+    }  
 
     // Update is called once per frame
     void Update()
     {
+
+        backgroundRenderer.material.mainTextureOffset = new Vector2(0, Time.realtimeSinceStartup * scrollSpeedY);
+
+        /*
+        // keep track the time on each frame
         transitionTimer += Time.deltaTime;
+
+        
+
+        if(transitionTimer >= changeInterval)
+        {
+            transitionTimer = 0.0f;
+            //currentBackgroundIndex = (currentBackgroundIndex + 1)   % backgrounds.Length;
+            //nextBackgroundIndex = (nextBackgroundIndex + 1) % backgrounds.Length;
+        }
 
         // calculate the time bewteen the current background and the next one
         float t = transitionTimer / changeInterval;
 
-        if(t > 1.0f)
-        {
-            transitionTimer = 0.0f;
-            currentBackgroundIndex = (currentBackgroundIndex + 1)   % backgrounds.Length;
-            nextBackgroundIndex = (nextBackgroundIndex + 1) % backgrounds.Length;
-        }
-
+        // changing degree
         backgroundRenderer.material.Lerp(backgrounds[currentBackgroundIndex], backgrounds[nextBackgroundIndex], t);
+    
+        */
     }
 }
