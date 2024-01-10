e5f1acfda813ec72422729281611d6b62076df08
JANGJAGUAR <jangjaguar@gmail.com>
2024-01-10 22:45:07 +0900

    feat: Test

---
+++ Assets/Soccer_Stadiums_Extension_Pack/AssetBundleResources/Scripts/FogController.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+#if UNITY_EDITOR
+
+using UnityEditor;
+
+[CustomEditor(typeof(FogController))]
+public class FogControllerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        var fog = target as FogController;
+
+        EditorGUI.BeginChangeCheck();
+
+        fog.isFogActive = EditorGUILayout.Toggle("フォグの有効/無効", fog.isFogActive);
+
+        fog.fogColor = EditorGUILayout.ColorField("RGB", fog.fogColor);
+
+        fog.fogMode = (FogMode)EditorGUILayout.EnumPopup(fog.fogMode);
+
+        fog.fogStartDistance = EditorGUILayout.FloatField("Start",fog.fogStartDistance);
+
+        fog.fogEndDistance = EditorGUILayout.FloatField("End", fog.fogEndDistance);
+
+        if (EditorGUI.EndChangeCheck())
+        {
+            fog.SetFog();
+        }
+
+        
+    }
+}
+
+#endif
+
+public class FogController:MonoBehaviour {
+
+    public bool isFogActive;
+
+    public Color fogColor;
+
+    public FogMode fogMode;
+
+    public float fogStartDistance;
+
+    public float fogEndDistance;
+
+    /*[Button("SetFog","反映")]
+    public int SetFogButton;*/
+
+    public void Start() {
+        SetFog();
+    }
+
+    public void SetFog()
+    {
+        RenderSettings.fog = isFogActive;
+        RenderSettings.fogColor = fogColor;
+        RenderSettings.fogMode = fogMode;
+        RenderSettings.fogStartDistance = fogStartDistance;
+        RenderSettings.fogEndDistance = fogEndDistance;
+    }
+
+}
---
+++ Assets/Soccer_Stadiums_Extension_Pack/AssetBundleResources/Scripts/Scrollanimation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UVScroll : MonoBehaviour {
+    
+    [SerializeField]
+    private float scrollSpeedX = 0.1f;
+
+    [SerializeField]
+    private float scrollSpeedY = 0.1f;
+    
+	void Start () {
+		GetComponent<Renderer>().sharedMaterial.SetTextureOffset("_MainTex", Vector2.zero);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		 var x = Mathf.Repeat(Time.time * scrollSpeedX, 1);
+        var y = Mathf.Repeat(Time.time * scrollSpeedY, 1);
+
+        var offset = new Vector2(x, y);
+        
+        GetComponent<Renderer>().sharedMaterial.SetTextureOffset("_MainTex", offset);
+	}
+}
